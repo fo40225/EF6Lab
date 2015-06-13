f238f32e590d0fd75ba28ca8dc29ee9f29e82dce
fo40225 <fo40225@hotmail.com>
2015-06-13 17:52:38 +0800

    import ModelFirst database project

---
+++ ModelFirst.Database/dbo/Tables/BlogSet.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[BlogSet] (
+    [Id]   INT            IDENTITY (1, 1) NOT NULL,
+    [Name] NVARCHAR (MAX) NOT NULL,
+    [Url]  NVARCHAR (MAX) NULL,
+    CONSTRAINT [PK_BlogSet] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
---
+++ ModelFirst.Database/dbo/Tables/PostSet.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[PostSet] (
+    [Id]      INT            IDENTITY (1, 1) NOT NULL,
+    [Title]   NVARCHAR (MAX) NOT NULL,
+    [Content] NVARCHAR (MAX) NOT NULL,
+    [BlogId]  INT            NOT NULL,
+    CONSTRAINT [PK_PostSet] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_BlogPost] FOREIGN KEY ([BlogId]) REFERENCES [dbo].[BlogSet] ([Id])
+);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_FK_BlogPost]
+    ON [dbo].[PostSet]([BlogId] ASC);
+
