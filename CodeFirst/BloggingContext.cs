8e37971606749e67b44d84d7317f11b02c334600
fo40225 <fo40225@hotmail.com>
2015-06-13 20:52:13 +0800

    Fluent API

--- CodeFirst/BloggingContext.cs
+++ CodeFirst/BloggingContext.cs
@@ -37,5 +37,12 @@
             get;
             set;
         }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>()
+                .Property(u => u.DisplayName)
+                .HasColumnName("display_name");
+        }
     }
 }
