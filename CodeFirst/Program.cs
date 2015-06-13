1c0a5ee1e647ec620dbce13c8410562282c9de5d
fo40225 <fo40225@hotmail.com>
2015-06-13 20:45:58 +0800

    CodeFirst main code

--- CodeFirst/Program.cs
+++ CodeFirst/Program.cs
@@ -6,10 +6,35 @@ using System.Threading.Tasks;
 
 namespace CodeFirst
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            using (var db = new BloggingContext())
+            {
+                // Create and save a new Blog
+                Console.Write("Enter a name for a new Blog: ");
+                var name = Console.ReadLine();
+
+                var blog = new Blog
+                {
+                    Name = name
+                };
+                db.Blogs.Add(blog);
+                db.SaveChanges();
+
+                // Display all Blogs from the database
+                var query = db.Blogs;
+
+                Console.WriteLine("All blogs in the database:");
+                foreach (var item in query)
+                {
+                    Console.WriteLine(item.Name);
+                }
+
+                Console.WriteLine("Press any key to exit...");
+                Console.ReadKey();
+            }
         }
     }
 }
