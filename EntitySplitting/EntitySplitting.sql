a2946037f8dd667f166da5e990278e0f7f929f49
fo40225 <fo40225@hotmail.com>
2015-06-13 19:26:16 +0800

    create sample database

---
+++ EntitySplitting/EntitySplitting.sql
@@ -0,0 +1,18 @@
+﻿CREATE DATABASE [EntitySplitting]
+
+USE [EntitySplitting]
+
+CREATE TABLE [dbo].[Person] (
+[PersonId] INT IDENTITY (1, 1) NOT NULL,
+[FirstName] NVARCHAR (200) NULL,
+[LastName] NVARCHAR (200) NULL,
+CONSTRAINT [PK_Person] PRIMARY KEY CLUSTERED ([PersonId] ASC)
+);
+
+CREATE TABLE [dbo].[PersonInfo] (
+[PersonId] INT NOT NULL,
+[Email] NVARCHAR (200) NULL,
+[Phone] NVARCHAR (50) NULL,
+CONSTRAINT [PK_PersonInfo] PRIMARY KEY CLUSTERED ([PersonId] ASC),
+CONSTRAINT [FK_Person_PersonInfo] FOREIGN KEY ([PersonId]) REFERENCES [dbo].[Person] ([PersonId]) ON DELETE CASCADE
+);
