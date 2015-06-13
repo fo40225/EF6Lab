f2a7753daf3c3cdc03c9eb2f64a50ab0bda7026b
fo40225 <fo40225@hotmail.com>
2015-06-13 17:57:08 +0800

    update database project

---
+++ ModelFirst.Database/dbo/Tables/UserSet.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[UserSet] (
+    [UserName]    NVARCHAR (50)  NOT NULL,
+    [DisplayName] NVARCHAR (MAX) NOT NULL,
+    CONSTRAINT [PK_UserSet] PRIMARY KEY CLUSTERED ([UserName] ASC)
+);
+
