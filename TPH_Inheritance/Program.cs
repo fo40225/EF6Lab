972f6637335d1869a2488749f128934c64673a67
fo40225 <fo40225@hotmail.com>
2015-06-13 19:10:02 +0800

    TPH Inheritance main code

--- TPH_Inheritance/Program.cs
+++ TPH_Inheritance/Program.cs
@@ -10,6 +10,26 @@ namespace TPH_Inheritance
     {
         static void Main(string[] args)
         {
+            using (var db = new SchoolEntities())
+            {
+                Console.WriteLine("All people:");
+                foreach (var person in db.People)
+                {
+                    Console.WriteLine("    {0} {1}", person.FirstName, person.LastName);
+                }
+
+                Console.WriteLine("Instructors only: ");
+                foreach (var person in db.People.OfType<Instructor>())
+                {
+                    Console.WriteLine("    {0} {1}", person.FirstName, person.LastName);
+                }
+
+                Console.WriteLine("Students only: ");
+                foreach (var person in db.People.OfType<Student>())
+                {
+                    Console.WriteLine("    {0} {1}", person.FirstName, person.LastName);
+                }
+            }
         }
     }
 }
