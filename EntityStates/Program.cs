70fdc78725ae67d9f3ad25c54b783f66d09f5aba
fo40225 <fo40225@hotmail.com>
2015-06-13 20:21:35 +0800

    Update without load

--- EntityStates/Program.cs
+++ EntityStates/Program.cs
@@ -6,10 +6,23 @@ using System.Threading.Tasks;
 
 namespace EntityStates
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            // Update without load
+            using (var db = new BlogEntities())
+            {
+                var existBlog = new BlogSet()
+                {
+                    Id = 3,
+                    Name = "kk"
+                };
+
+                db.BlogSets.Attach(existBlog);
+                db.Entry(existBlog).Property(x => x.Name).IsModified = true;
+                db.SaveChanges();
+            }
         }
     }
 }
