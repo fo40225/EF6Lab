17611cd6cf929a243c73c1dfd31d78589bb1f850
fo40225 <fo40225@hotmail.com>
2015-06-13 19:36:45 +0800

    TableSplitting main code

--- TableSplitting/Program.cs
+++ TableSplitting/Program.cs
@@ -6,10 +6,41 @@ using System.Threading.Tasks;
 
 namespace TableSplitting
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            using (var db = new SchoolEntities())
+            {
+                Person person = new Person()
+                {
+                    FirstName = "Kimberly",
+                    LastName = "Morgan",
+                    Discriminator = "Instructor",
+                };
+
+                person.HireInfo = new HireInfo()
+                {
+                    HireDate = DateTime.Now
+                };
+
+                // Add the new person to the context.
+                db.People.Add(person);
+
+                // Insert a row into the Person table.
+                db.SaveChanges();
+
+                // Execute a query against the Person table.
+                // The query returns columns that map to the Person entity.
+                var existingPerson = db.People.FirstOrDefault();
+
+                // Execute a query against the Person table.
+                // The query returns columns that map to the Instructor entity.
+                var hireInfo = existingPerson.HireInfo;
+
+                Console.WriteLine("{0} was hired on {1}",
+                    existingPerson.LastName, hireInfo.HireDate);
+            }
         }
     }
 }
