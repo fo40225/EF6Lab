1786468b537229c6e2892bcf6a3327144b76740f
fo40225 <fo40225@hotmail.com>
2015-06-13 20:45:17 +0800

    add entities class

---
+++ CodeFirst/Blog.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CodeFirst
+{
+    public class Blog
+    {
+        public int BlogId
+        {
+            get;
+            set;
+        }
+
+        public string Name
+        {
+            get;
+            set;
+        }
+
+        public virtual List<Post> Posts
+        {
+            get;
+            set;
+        }
+    }
+}
