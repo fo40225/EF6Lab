220adc04f48c01c5b805f81f4f4a6b88910e5be3
fo40225 <fo40225@hotmail.com>
2015-06-13 19:46:21 +0800

    LinqToEntities main code

--- LinqToEntities/Program.cs
+++ LinqToEntities/Program.cs
@@ -6,10 +6,32 @@ using System.Threading.Tasks;
 
 namespace LinqToEntities
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            using (var db = new SchoolEntities())
+            {
+                // Query for all Person with names starting with B
+                var people = db.People.Where(x => x.FirstName.StartsWith("B"));
+
+                // Query for the Person named Roger
+                var person = db.People.Where(b => b.FirstName == "Roger").FirstOrDefault();
+
+                // Will hit the database
+                var person2 = db.People.Find(3);
+
+                // Will return the same instance without hitting the database
+                var person2Again = db.People.Find(3);
+
+                db.People.Add(new Person
+                {
+                    PersonID = -1
+                });
+
+                // Will find the new person even though it does not exist in the database
+                var newPerson = db.People.Find(-1);
+            }
         }
     }
 }
